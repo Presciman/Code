35e92200ba0134a8704e39c44c9e1157832f0107
Eric Sun <sunbaixi@163.com>
2020-10-28 11:10:28 -0700

    Update runMatlab.sh

--- Matlab_codes/runMatlab.sh
+++ Matlab_codes/runMatlab.sh
@@ -2,7 +2,7 @@
 #SBATCH -J tensorD            # job name
 #SBATCH -e matlabjob.%j.err       # error file name 
 #SBATCH -o matlabjob.%j.out       # output file name 
-#SBATCH -N 4                      # request 1 node
+#SBATCH -N 1                      # request 1 node
 #SBATCH -n 16                   # request all 16 cores
 #SBATCH -p development
 #SBATCH -t 02:00:00
